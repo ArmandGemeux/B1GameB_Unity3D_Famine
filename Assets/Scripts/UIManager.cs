f82170870a521cacff6440b7c34f624e4e356758
ArmandGemeux <armandgemeux@hotmail.fr>
2019-03-07 13:54:48 +0100

    Envoie de l'UI pour Box et Timer, renommage de la scène en <Famine> et gros bisous

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -5,9 +5,9 @@ using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-
-    public float timerValue = 30f;
-    private float currentTimerValue = 0f;
+    
+    public float timerValue = 90f; // valeur de départ du timer
+    private float currentTimerValue = 0f; // valeur actuelle du timer
     public Text timerText;
 
     public static UIManager Singleton;
@@ -34,8 +34,8 @@ public class UIManager : MonoBehaviour
     void Update()
     {
 
-        currentTimerValue -= Time.deltaTime;
-        timerText.text = currentTimerValue.ToString("0.0");
+        currentTimerValue -= Time.deltaTime; // réduit la valeur du timer en fonction du temps
+        timerText.text = currentTimerValue.ToString("0.0"); // Affiche la valeur du timer
     }
 
     public void AddTenSeconds()
