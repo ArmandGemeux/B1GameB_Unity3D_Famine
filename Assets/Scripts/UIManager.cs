548e50bcca6f09e60592aa3ac90d6408c9da29ce
Dylan_Schumacker <dylan.schumacker@outlook.fr>
2019-03-18 14:04:33 +0100

    *.*

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -3,15 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.Threading;
 
 public class UIManager : MonoBehaviour
 {
-    
+    private bool activeOnce = false;
     public float timerValue = 90f; // valeur de départ du timer
     private float currentTimerValue = 0f; // valeur actuelle du timer
     public Text timerText;
 
     public static UIManager UI_Singleton;
+
+    //Countdown
+    public float startTimer;
+    public Text startText;
+    public GameObject Countdown;
+
 
     private void Awake()
     {
@@ -34,9 +41,27 @@ public class UIManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        currentTimerValue -= Time.deltaTime; // réduit la valeur du timer en fonction du temps
-        timerText.text = currentTimerValue.ToString("0.0"); // Affiche la valeur du timer
-        if(currentTimerValue <= 0f)
+        //Le timer s'enclenche
+        startTimer -= Time.deltaTime;
+        startText.text = (startTimer).ToString("0");
+
+        if (activeOnce == false)
+        {
+
+            if (startTimer <= 0)
+            {              
+                Countdown.SetActive(!Countdown.activeSelf);
+                activeOnce = true;
+            }
+        }
+
+        if (activeOnce == true)
+        {
+            currentTimerValue -= Time.deltaTime; // réduit la valeur du timer en fonction du temps
+            timerText.text = currentTimerValue.ToString("0.0"); // Affiche la valeur du timer
+        }
+
+            if (currentTimerValue <= 0f)
         {
             SceneManager.LoadScene(2);
         }
@@ -47,4 +72,5 @@ public class UIManager : MonoBehaviour
     {
         currentTimerValue += 10f;
     }
+    
 }
