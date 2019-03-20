9ea080190c53e4d54cb41b6b6e9a1716b9c6ebe6
ArmandGemeux <armandgemeux@hotmail.fr>
2019-03-20 11:22:38 +0100

    Ajout des animations de base de l'UI BOX

--- Assets/Scripts/Box_Atome_Animation.cs
+++ Assets/Scripts/Box_Atome_Animation.cs
@@ -4,29 +4,14 @@ using UnityEngine;
 
 public class Box_Atome_Animation : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
-    {
-
-
-    }
-
-        void OnMouseOver()
-        {
-            //If your mouse hovers over the GameObject with the script attached, output this message
-            Debug.Log("Mouse is over GameObject.");
-        }
-
-        void OnMouseExit()
-        {
-            //The mouse is no longer hovering over the GameObject so output this message each frame
-            Debug.Log("Mouse is no longer on GameObject.");
-        }
-
-    }
+		
+	}
+}
